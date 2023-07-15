a37b71a78ec529fc2940c19166f07cffbd6c1830
GangemiLorenzo <lorenzo.gangemi.developer@gmail.com>
2023-07-15 16:42:23 +0200

    feat: pokemon_widget changes

--- lib/pokedex/view/pokemon_widget.dart
+++ lib/pokedex/view/pokemon_widget.dart
@@ -8,10 +8,10 @@ class PokemonWidget extends StatelessWidget {
   final bool isCatched;
 
   const PokemonWidget({
-    super.key,
+    Key? key,
     required this.pokemon,
     this.isCatched = false,
-  });
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -20,37 +20,42 @@ class PokemonWidget extends StatelessWidget {
       color: pokemon.color,
       elevation: isCatched ? 4 : 0,
       child: InkWell(
-        onTap: () => {
+        onTap: () {
           BlocProvider.of<PokedexBloc>(context).add(
             CatchPokemonEvent(
               id: pokemon.index,
             ),
-          ),
+          );
         },
         borderRadius: BorderRadius.circular(8.0),
-        child: Row(
-          children: [
-            const SizedBox(width: 10.0),
-            Image.network(
-              pokemon.spriteUrl,
-              width: 80,
-              height: 80,
-              scale: 1,
-            ),
-            const SizedBox(width: 10.0),
-            Text(
-              pokemon.name,
-              style: TextStyle(
-                fontSize: 20.0,
-                color: Theme.of(context).colorScheme.onPrimaryContainer,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Image.network(
+                pokemon.spriteUrl,
+                width: 80,
+                height: 80,
+                scale: 1,
               ),
-            ),
-            const SizedBox(width: 10.0),
-            if (isCatched)
-              const Icon(
-                Icons.check,
+              const SizedBox(width: 8.0),
+              Expanded(
+                child: Text(
+                  pokemon.name,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Theme.of(context).colorScheme.onPrimaryContainer,
+                  ),
+                ),
               ),
-          ],
+              if (isCatched)
+                const Icon(
+                  Icons.check,
+                ),
+              const SizedBox(width: 8.0),
+            ],
+          ),
         ),
       ),
     );
